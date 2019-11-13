d4abd5625d5671205f2a9a8ae1cdceb6cbcd5c30
nyeee <911152732@qq.com>
2019-11-13 22:27:49 +0800

    file

---
+++ src/components/common-views/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () =>
+    <div className="ant-col-24 app-footer">
+        <span>********有限公司 All Rights Reserved</span>
+    </div>;
+
+export default Footer;
